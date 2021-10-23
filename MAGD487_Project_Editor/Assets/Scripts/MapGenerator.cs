8240d5f38cfcbf2f453a077b0014f8900df885ce
Dylan <dylanoelke.business@gmail.com>
2021-10-22 20:01:04 -0500

    Map Generation Stuff

--- MAGD487_Project_Editor/Assets/Scripts/MapGenerator.cs
+++ MAGD487_Project_Editor/Assets/Scripts/MapGenerator.cs
@@ -1,3 +1,4 @@
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,23 +8,36 @@ public class MapGenerator : MonoBehaviour
     [SerializeField] List<GameObject> mapBlocks;
     [SerializeField] int blocksInX, blocksInY;
     [SerializeField] int blockOffset;
+    public List<MapBlock> mapBlocksSpawned = new List<MapBlock>();
+    [SerializeField] Transform player;
     // Start is called before the first frame update
     void Start()
     {
         //Generate Map
         GenerateMap();
+
+        //Spawn Player
+        SpawnPlayer();
     }
 
     void GenerateMap()
     {
-        for (int i = 0; i < blocksInX; i++)
+        for (int i = 0; i < blocksInY; i++)
         {
-            for (int j = 0; j < blocksInY; j++)
+            for (int j = 0; j < blocksInX; j++)
             {
                 GameObject block = PickRandomMapBlock();
-                Instantiate(block, new Vector3(i * blockOffset, j * blockOffset) + this.transform.position, Quaternion.identity);
+                GameObject g = Instantiate(block, new Vector3(i * blockOffset, j * blockOffset) + this.transform.position, Quaternion.identity);
+                mapBlocksSpawned.Add(g.GetComponent<MapBlock>());
+                
             }
         }
+    }
+
+    void SpawnPlayer()
+    {
+        int rand = Random.Range(0, mapBlocksSpawned.Count);
+        player.position = mapBlocksSpawned[rand].playerSpawns[Random.Range(0, mapBlocksSpawned[rand].playerSpawns.Count)].position;
     }
     private GameObject PickRandomMapBlock()
     {
