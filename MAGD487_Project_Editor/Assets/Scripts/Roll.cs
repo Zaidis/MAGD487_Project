c4d422c443c200e9b5bf48bfc20e8f25f9402a31
Derek Woodard <thedjw191@gmail.com>
2021-10-23 18:29:30 -0500

    roll script work

--- MAGD487_Project_Editor/Assets/Scripts/Roll.cs
+++ MAGD487_Project_Editor/Assets/Scripts/Roll.cs
@@ -6,18 +6,37 @@ public class Roll : MonoBehaviour
 {
     Rigidbody2D rb;
     [SerializeField] GroundDetector groundDetector;
-    public bool wasGrounded;
-    
+    public bool isGrounded;
+    private bool wantToRoll = false;
+    private bool rolling = false;
+    private float direction;
+    [SerializeField] float rollForce;
+    [SerializeField] float maxSpeed;
 
     private void Awake() {
         rb = GetComponent<Rigidbody2D>();
     }
-    void Start()
-    {
-        
-    }
-    void Update() {
+    private void Update() {
         if (groundDetector.grounded)
-            wasGrounded = true;
+            isGrounded = true;
+    }
+    private void FixedUpdate(){        
+        if (isGrounded && wantToRoll && !rolling) { //apply force in direction of analog stick
+            rolling = true;
+            rb.AddForce(new Vector2(rollForce * (direction >= 0 ? 1 : -1), 0), ForceMode2D.Impulse);
+            Debug.Log(rb.velocity.x);
+        }
+        if(Mathf.Abs(rb.velocity.x) < maxSpeed)
+            rolling = false;
+    }
+    public void RollDirection(InputAction.CallbackContext callbackContext){
+        direction = callbackContext.ReadValue<Vector2>().x;
+    }
+    public void RollInput(InputAction.CallbackContext callbackContext){
+        Debug.Log("Roll");
+        if (callbackContext.performed)
+            wantToRoll = true;
+        if (callbackContext.canceled)
+            wantToRoll = false;
     }
 }
