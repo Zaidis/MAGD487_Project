5c4bbf8280735c51b668c2c880d1d70e1bacdd13
Derek Woodard <thedjw191@gmail.com>
2021-10-23 18:56:07 -0500

    merged roll script into movement

--- MAGD487_Project_Editor/Assets/Scripts/PlayerMovement.cs
+++ MAGD487_Project_Editor/Assets/Scripts/PlayerMovement.cs
@@ -7,12 +7,16 @@ public class PlayerMovement : MonoBehaviour{
     Vector2 movement;
     [SerializeField]
     float speed;
+
+    [Space][SerializeField] GroundDetector groundDetector; //roll elements
+    private bool wantToRoll = false;
+    private bool rolling = false;
+    [SerializeField] float rollForce;
+
     void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
     }
-   
-    // Update is called once per frame
     void Update()
     {
         movement = movement.normalized;
@@ -21,11 +25,26 @@ public class PlayerMovement : MonoBehaviour{
     private void FixedUpdate()
     {
         rb.velocity = new Vector3(movement.x * speed, rb.velocity.y);
+
+        if (groundDetector.grounded && wantToRoll && !rolling)
+        { //add timer, adjust force manually, cancel player input left and right, timer
+            rolling = true;
+            rb.AddForce(new Vector2(rollForce * (movement.x >= 0 ? 1 : -1), 0), ForceMode2D.Impulse);
+            Debug.Log(rb.velocity.x);
+        }
+        if (Mathf.Abs(rb.velocity.x) <= speed)//change to timer system
+            rolling = false;
     }
 
     public void Move(InputAction.CallbackContext callbackContext)
     {
         movement = new Vector2(callbackContext.ReadValue<Vector2>().x, callbackContext.ReadValue<Vector2>().y);
     }
-
+    public void RollInput(InputAction.CallbackContext callbackContext)
+    {
+        if (callbackContext.performed)
+            wantToRoll = true;
+        if (callbackContext.canceled)
+            wantToRoll = false;
+    }
 }
