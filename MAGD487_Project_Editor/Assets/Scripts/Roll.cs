934a91b39fefd6473abc8d9ba4614e0ca96b66ab
Derek Woodard <thedjw191@gmail.com>
2021-10-23 13:08:00 -0500

    Started Roll Script

---
+++ MAGD487_Project_Editor/Assets/Scripts/Roll.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+public class Roll : MonoBehaviour
+{
+    Rigidbody2D rb;
+    [SerializeField] GroundDetector groundDetector;
+    public bool wasGrounded;
+
+    private void Awake() {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    void Start()
+    {
+        
+    }
+    void Update() {
+        if (groundDetector.grounded)
+            wasGrounded = true;
+    }
+}
