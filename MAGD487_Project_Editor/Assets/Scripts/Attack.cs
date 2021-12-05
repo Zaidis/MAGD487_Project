69c7908953a193d34ea9e2f6bb9d44cfbf5d09b9
djw191 <50028101+djw191@users.noreply.github.com>
2021-12-05 07:31:01 -0600

    Added basic grapple launch functionality

--- MAGD487_Project_Editor/Assets/Scripts/Attack.cs
+++ MAGD487_Project_Editor/Assets/Scripts/Attack.cs
@@ -10,6 +10,10 @@ public class Attack : MonoBehaviour
     public bool AttackInputReceived;
     private GroundDetector groundDetector;
     private InventoryManager im;
+    private Camera mainCam;
+
+    public GameObject shot;
+    public float speed;
     private void Awake()
     {
         canReceiveAttackInput = true;
@@ -17,13 +21,23 @@ public class Attack : MonoBehaviour
         instance = this;
         groundDetector = GetComponentInChildren<GroundDetector>();
         im = InventoryManager.instance;
+        mainCam = GameObject.Find("Camera").GetComponent<Camera>();
     }
     public void AttackInput(InputAction.CallbackContext callbackContext)
     {
         if(callbackContext.performed) {
             if(canReceiveAttackInput && groundDetector.grounded) {
-                weaponType wt = im.CheckCurrentItemForWeaponType();
-                //TODO Change Animation based on weapon type;
+                weaponType wt = im.CheckCurrentItemForWeaponType(); //Move to animation handler script?
+                if(wt == weaponType.none) {
+                    //no attack animation
+                } if(wt == weaponType.dagger) {
+                    //dagger attack animation
+                } if(wt == weaponType.grapple) {
+                    //no attack animation
+                    Shoot();
+                } if(wt == weaponType.sword) {
+                    //sword attack animation
+                }
                 AttackInputReceived = true;
                 canReceiveAttackInput = false;
             } else {
@@ -38,5 +52,14 @@ public class Attack : MonoBehaviour
         } else {
             canReceiveAttackInput = false;
         }
-    }    
+    }
+    public void Shoot()
+    {
+        Vector3 direction;
+        direction = mainCam.ScreenToWorldPoint(new Vector3(Mouse.current.position.ReadValue().x, Mouse.current.position.ReadValue().y, 0.0f));
+        direction = mainCam.ScreenToWorldPoint(direction);
+        direction = direction - transform.position;
+        GameObject grappleShot = Instantiate(shot, transform.position, Quaternion.Euler(Vector3.zero));
+        grappleShot.GetComponent<Rigidbody2D>().velocity = new Vector2(direction.x * speed, direction.y * speed);
+    }
 }
