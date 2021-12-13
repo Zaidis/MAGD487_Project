1efcd2b09b4cc2603cd6974a7ad7ab68cd9eba3a
djw191 <50028101+djw191@users.noreply.github.com>
2021-12-13 11:10:16 -0600

    Fixed animation artifacts and bug where you could teleport into wall

--- MAGD487_Project_Editor/Assets/Scripts/TransitionBehavior.cs
+++ MAGD487_Project_Editor/Assets/Scripts/TransitionBehavior.cs
@@ -4,20 +4,39 @@ using UnityEngine;
 
 public class TransitionBehavior : StateMachineBehaviour
 {
+    public PlayerAnimationController PAC;
+    private SpriteRenderer SR;
+    private RaycastHit2D hit;
     [SerializeField] private int animNum;
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
         PlayerMovement.instance.canMove = true;
+        PAC = GameObject.Find("Graphic").GetComponent<PlayerAnimationController>();
+        SR = GameObject.Find("Graphic").GetComponent<SpriteRenderer>();
     }
 
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
     override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        if(Attack.instance.AttackInputReceived) {
-            animator.SetTrigger("Attack" + animNum);
-            Attack.instance.InputManager();
-            Attack.instance.AttackInputReceived = false;
+        
+        if (Attack.instance.AttackInputReceived) {
+            if(PAC.wt == weaponType.sword && animNum == 2) //Stops player from teleporting into wall genius code, not spaghetti
+            {
+                hit = Physics2D.Raycast(SR.transform.position, Vector2.right * (SR.flipX ? -1f : 1f));
+                if(hit.distance >= 3)
+                {
+                    animator.SetTrigger("Attack" + animNum);
+                    Attack.instance.InputManager();
+                    Attack.instance.AttackInputReceived = false;
+                }
+            }
+            else
+            {
+                animator.SetTrigger("Attack" + animNum);
+                Attack.instance.InputManager();
+                Attack.instance.AttackInputReceived = false;
+            }            
         }
     }
 
