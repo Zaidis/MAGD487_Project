02e634a43d25b3c2242c0d808e91b9f7a670e720
Dylan <dylanoelke.business@gmail.com>
2021-12-12 20:03:35 -0600

    Cool particle lights

--- MAGD487_Project_Editor/Assets/Scripts/AI/ProjectileMovement.cs
+++ MAGD487_Project_Editor/Assets/Scripts/AI/ProjectileMovement.cs
@@ -7,6 +7,7 @@ public class ProjectileMovement : MonoBehaviour
     [SerializeField] Rigidbody2D rb;
     [SerializeField] float speed;
     [SerializeField] float damage;
+    [SerializeField] bool attachedParticles = false;
     private void Start()
     {
         rb.velocity = new Vector2(speed * this.transform.localScale.x, 0);
@@ -17,10 +18,26 @@ public class ProjectileMovement : MonoBehaviour
         if(collision.TryGetComponent<Damageable>(out Damageable damageable))
         {
             damageable.Damage(damage);
+
+            if (attachedParticles)
+                DestroyParticles();
+
             Destroy(this.gameObject);
         }else if (collision.CompareTag("Ground"))
         {
+            if (attachedParticles)
+                DestroyParticles();
+
             Destroy(this.gameObject);
         }
+    }
+
+    void DestroyParticles()
+    {
+        ParticleSystem s = GetComponentInChildren<ParticleSystem>();
+        s.transform.SetParent(null);
+        ParticleSystem.EmissionModule f = s.emission;
+        f.enabled = false;
+        Destroy(s.gameObject, 5);
     }
 }
